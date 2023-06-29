29c1b6a7ba949bfeff8813690f29ae9fe83d2b4b
Romek <kharkov501@gmail.com>
2023-06-29 19:05:37 +0200

    Initial
    Signed-off-by: Romek <kharkov501@gmail.com>

---
+++ ggsdk/ggsdk.h
@@ -0,0 +1,12 @@
+//
+//  ggsdk.h
+//  ggsdk
+//
+//  Created by user241914 on 6/29/23.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ggsdk : NSObject
+
+@end
---
+++ ggsdk/ggsdk.m
@@ -0,0 +1,12 @@
+//
+//  ggsdk.m
+//  ggsdk
+//
+//  Created by user241914 on 6/29/23.
+//
+
+#import "ggsdk.h"
+
+@implementation ggsdk
+
+@end
